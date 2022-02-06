3484fef2c125c846335ad4169456f5a5337f3760
Codekacode <karem.quiroz.m@gmail.com>
2022-02-06 11:57:24 -0500

    Create petitions http get and post

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-import express, { Application } from 'express'; 
+import express from "express"; 
+import { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+app.use(express.json())
+
+app.get('/users', (req: Request, res: Response) => {
+
+    res.status(201).json({result:["1"]})
+});
+
+app.post('/users/:param', (req: Request, res: Response) => {
+
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    res.status(200).send({});
+})
 
 app.listen(3000, () => console.log("server hola listo"))
